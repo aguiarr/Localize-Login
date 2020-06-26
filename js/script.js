a7bb8d4dde3b7ff443fb4f4f843bb3e38460dea2
aguiarr <aguiartgv@gmail.com>
2020-06-26 00:23:58 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+    var btn = document.getElementById('btn');
+    var label = document.getElementById('singup');
+   
+
+    label.addEventListener('click', function(){
+        document.getElementById('a').style.display = 'block';
+})
