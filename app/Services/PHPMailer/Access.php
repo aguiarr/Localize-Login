aee0f90924d3c240372c2c14ea86002e6d386c4c
Matheus Aguiar <matheus.aguiar@apiki.com>
2021-03-29 11:50:30 -0300

    feat: add arquivo access ao gitignore

--- app/Services/PHPMailer/Access.php
+++ app/Services/PHPMailer/Access.php
@@ -5,9 +5,9 @@ namespace Localize\Services\PHPMailer;
 class Access
 {
 
-    public static $mail_host            = '';
-    public static $mail_port            = '';
+    public static $mail_host            = 'smtp.gmail.com';
+    public static $mail_port            = '465';
     public static $mail_smtp_secure     = 'ssl';
-    public static $mail_username        = '';
-    public static $mail_password        = '';
+    public static $mail_username        = 'mailtesteraguiar@gmail.com';
+    public static $mail_password        = '@A3506b3';
 }
